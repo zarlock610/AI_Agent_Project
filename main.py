d6d0a66b8f3168584ea2401662ebe47bef3b9bc5
zarlock610 <andyroo.dixon@live.com>
2025-09-10 17:09:17 -0400

    Added hardcoded call to gemini api and it runs

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
     print("Hello from ai-agent-bootdev!")
 
 
 if __name__ == "__main__":
     main()
+
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+
+
+client = genai.Client(api_key=api_key)
+
+GenCliRes = client.models.generate_content(model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+print(GenCliRes.text)
+ptokens = GenCliRes.usage_metadata.prompt_token_count
+rtokens = GenCliRes.usage_metadata.candidates_token_count
+print(f"Prompt tokens: {ptokens}\n"
+      f"Response tokens: {rtokens}")
