0a11437f4cab880790d89778b43a6e2c3d1be7c5
zarlock610 <andyroo.dixon@live.com>
2025-09-11 19:06:21 -0400

    --Added verbose tag and it functions

--- main.py
+++ main.py
@@ -4,24 +4,46 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-]
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
     if len(sys.argv) < 2:
         print("You must provide a prompt as a command line argument.", file=sys.stderr) 
-        sys.exit(1)  
-    else: print(f"Generating content for: {sys.argv[1]}")
-    GenCliRes = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages,)
-    print(GenCliRes.text)
-    ptokens = GenCliRes.usage_metadata.prompt_token_count
-    rtokens = GenCliRes.usage_metadata.candidates_token_count
-    print(f"Prompt tokens: {ptokens}\n"
-      f"Response tokens: {rtokens}")
+        sys.exit(1)
+    elif len(sys.argv) == 3:
+        if sys.argv[2] == "--verbose":
+            user_prompt = sys.argv[1]
+            messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+            ]
+            client = genai.Client(api_key=api_key)
+            GenCliRes = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages,)
+            ptokens = GenCliRes.usage_metadata.prompt_token_count
+            rtokens = GenCliRes.usage_metadata.candidates_token_count
+            
+            print(f"Response: {GenCliRes.text}")
+            print(f"User prompt: {sys.argv[1]}")
+            print(f"Prompt tokes: {ptokens}")
+            print(f"Response tokens: {rtokens}")
+        else: print("Improper Argument. Use --verbose for detailed output.", file=sys.stderr)  
+    else: 
+        user_prompt = sys.argv[1]
+        messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
+        client = genai.Client(api_key=api_key)
+        GenCliRes = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages,)
+        
+        print(f"Response: {GenCliRes.text}")
+    
+   
+    
+    
+    
+    
+    
+    
 
 
 if __name__ == "__main__":
