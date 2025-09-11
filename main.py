d37fd5eecb74dad7ecc45c95a208fcba5577f0fe
zarlock610 <andyroo.dixon@live.com>
2025-09-10 20:28:40 -0400

    --Updated to allow user prompts --Gives error message if no prompt supplied

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
-    print("Hello from ai-agent-bootdev!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    if len(sys.argv) < 2:
+        print("You must provide a prompt as a command line argument.", file=sys.stderr) 
+        sys.exit(1)  
+    else: print(f"Generating content for: {sys.argv[1]}")
+    GenCliRes = client.models.generate_content(model='gemini-2.0-flash-001', contents=sys.argv[1])
+    print(GenCliRes.text)
+    ptokens = GenCliRes.usage_metadata.prompt_token_count
+    rtokens = GenCliRes.usage_metadata.candidates_token_count
+    print(f"Prompt tokens: {ptokens}\n"
+      f"Response tokens: {rtokens}")
 
 
 if __name__ == "__main__":
@@ -11,16 +24,6 @@ if __name__ == "__main__":
 
 
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
 
 
-client = genai.Client(api_key=api_key)
-
-GenCliRes = client.models.generate_content(model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
-print(GenCliRes.text)
-ptokens = GenCliRes.usage_metadata.prompt_token_count
-rtokens = GenCliRes.usage_metadata.candidates_token_count
-print(f"Prompt tokens: {ptokens}\n"
-      f"Response tokens: {rtokens}")
