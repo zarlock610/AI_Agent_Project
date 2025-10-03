167c3135fc97775610836615373165177256587c
zarlock610 <andyroo.dixon@live.com>
2025-10-02 21:01:00 -0400

    Finished get_file_contents.py and tests.py in ~/calculator

--- functions/get_file_contents.py
+++ functions/get_file_contents.py
@@ -1,15 +1,19 @@
-from config import MAX_FILE_SIZE
+from functions.config import MAX_FILE_SIZE
 import os
 
 def get_file_contents(working_directory, file_path):
     try:
-        if os.path.commonpath([working_directory, file_path]) != os.path.abspath(working_directory):
-            raise Exception(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
+        full_path = os.path.abspath(os.path.join(working_directory, file_path))#This sets a custom path that is the working directory(the floor) and the directory(where we are going to).
     except Exception as e:
-        return f"Error: Unable to validate file path within boundaries. Line 4. {str(e)}"
+        return f"Error: Unable to create absolute path. Line 6. {str(e)}"
     try:
-        if not os.path.isfile(file_path):
-            raise Exception(f'Error: File not found or is not a regular file: "{file_path}"')
+        if os.path.commonpath([full_path, os.path.abspath(working_directory)]) != os.path.abspath(working_directory):#error if full path isn't inside working directory; this is a validator
+            raise Exception(f'Error: Cannot list "{file_path}" as it is outside the permitted working directory. Line 10.')
+    except Exception as e:
+        return f'Error: Unable to validate path. Line 10. {str(e)}'
+    try:
+        if not os.path.isfile(full_path):
+            raise Exception(f'Error: File not found or is not a regular file: "{full_path}"')
     except Exception as e:
         return f"Error: Unable to validate file existence. Line 9. {str(e)}"
     try:
@@ -17,7 +21,14 @@ def get_file_contents(working_directory, file_path):
     except Exception as e:
         return f"Error: Unable to set MAX_CHARS from config. Line 16. {str(e)}"
     try:
-        with open(file_path, "r") as f:
-            file_content_string = f.read(MAX_CHARS)
+        with open(full_path, "r") as f:
+            full_content = f.read()
     except Exception as e:
         return f"Error: Unable to read file. Line 18. {str(e)}"
+    try:
+        if len(full_content) > 10000:
+            return full_content[:MAX_CHARS] + f'[...File "{full_path}" truncated at 10000 characters]'
+    except Exception as e:
+        return f"Error: Unable to give file truncation message. Line 25. {str(e)}"
+    return full_content[:MAX_CHARS]
+    
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_contents import get_file_contents
 
-print(get_files_info("calculator", "."))
-print(get_files_info("calculator", "pkg"))
-print(get_files_info("calculator", "/bin"))
-print(get_files_info("calculator", "../"))
+print(get_file_contents("calculator", "main.py"))
+print(get_file_contents("calculator", "pkg/calculator.py"))
+print(get_file_contents("calculator", "/bin/cat"))
+print(get_file_contents("calculator", "pkg/does_not_exist.py"))
 
