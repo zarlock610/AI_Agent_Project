1a9b2464ad0b579fae085f1d5f52441a62bbd3e3
zarlock610 <andyroo.dixon@live.com>
2025-10-02 21:24:19 -0400

    --write_file.py completed

---
+++ functions/write_file.py
@@ -0,0 +1,24 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+        full_path = os.path.abspath(os.path.join(working_directory, file_path))#This sets a custom path that is the working directory(the floor) and the directory(where we are going to).
+    except Exception as e:
+        return f"Error: Unable to create absolute path. Line 6. {str(e)}"
+    try:
+        if os.path.commonpath([full_path, os.path.abspath(working_directory)]) != os.path.abspath(working_directory):#error if full path isn't inside working directory; this is a validator
+            raise Exception(f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory. Line 10.')
+    except Exception as e:
+        return f'Error: Unable to validate path. Line 10. {str(e)}'
+    try:
+        if not os.path.exists(full_path):
+            os.makedirs(full_path)
+    except Exception as e:
+        return f"Error: Unable to create directories. Line 15. {str(e)}"
+    try:
+        with open(full_path, "w") as f:
+            f.write(content)
+    except Exception as e:
+        return f"Error: Unable to write to file. Line 20. {str(e)}"
+    else:
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
